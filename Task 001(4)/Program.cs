7f36f1924969254b1157d40949742ea8eecc518b
Anatoly <a.smorodin@bk.ru>
2022-12-22 00:28:15 +0300

    Task 004(1) error

---
+++ Task 001(4)/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход 
+//два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите превое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int z = 0;
+
+for (int z = a * a) 
+{
+    result = int z * int b;
+}
+Console.WriteLine(result);
+
