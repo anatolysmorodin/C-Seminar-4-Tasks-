7170e4a952d4b90d27a8c08cbf7a1588a1adbcb7
Anatoly <a.smorodin@bk.ru>
2022-12-22 17:04:04 +0300

    Task 001(4) fail

--- Task 001(4)/Program.cs
+++ Task 001(4)/Program.cs
@@ -1,15 +1,20 @@
 ﻿// Напишите цикл, который принимает на вход 
 //два числа (A и B) и возводит число A в натуральную степень B.
 
-Console.WriteLine("Введите превое число: ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
-int z = 0;
+Console.WriteLine("Введите первое число A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число B: ");
+int b = int.Parse(Console.ReadLine()!);
+int NS = a;
+int i = 1;
 
-for (int z = a * a) 
+while (i<b)
 {
-    result = int z * int b;
+    NS = NS*a;
+    i++;
 }
-Console.WriteLine(result);
+Console.WriteLine("Число А в степени B равно: " + NS);
+
+
+
 
---
+++ Task 002(4)/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+string i = 0;
+string summ = 0;
+
+for (string i=0; i < string_str.Lenght; i++)
+{
+    summ = summ + string sum;
+}
+Console.Write(summ);
