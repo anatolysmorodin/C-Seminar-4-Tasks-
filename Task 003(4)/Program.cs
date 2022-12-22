4a031c1214f5b63026ebf776db8d119eb3836fcc
Anatoly <a.smorodin@bk.ru>
2022-12-22 19:42:35 +0300

    Task 003(4) complete

---
+++ Task 003(4)/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 случайных чисел (-10, 10) и выводит их на экран.
+
+int[]array = new int[8];
+Console.Write("[");
+
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    string elements = Convert.ToString(array[i]);
+    int convert = Convert.ToInt32(elements);
+    Console.Write(convert.ToString() + ", ");
+}
+Console.WriteLine("]");
+
