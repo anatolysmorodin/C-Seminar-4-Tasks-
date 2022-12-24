e68e7c8887e23722851c5f31a4318bdbdc3857e3
anatoly <a.smorodin@bk.ru>
2022-12-24 18:36:53 +0300

    Task (6)001 failed

---
+++ Task (6)001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел: ");
+string inputText = Console.ReadLine();
+string[] stringArray = inputText.Split(' ');
+int[] array = new int[stringArray.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(stringArray[i]);    
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    Console.Write($"Введено чисел больше нуля: " + array[i] + " ");
+}
