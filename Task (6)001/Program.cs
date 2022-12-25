fad2d900267a7a64b2627e384fce70a4787e84ef
anatoly <a.smorodin@bk.ru>
2022-12-25 14:51:34 +0300

    Task (6)001 failed

--- Task (6)001/Program.cs
+++ Task (6)001/Program.cs
@@ -1,18 +1,39 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. 
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
+// Console.WriteLine("Введите числа через пробел: ");
+// string inputText = Console.ReadLine();
+// string[] stringArray = inputText.Split(' ');
+// int[] array = new int[stringArray.Length];
+
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = int.Parse(stringArray[i]);    
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > 0) 
+    
+//     Console.Write($"Введено чисел больше нуля: " + array[i] + " ");
+// }
+
 Console.WriteLine("Введите числа через пробел: ");
-string inputText = Console.ReadLine();
-string[] stringArray = inputText.Split(' ');
+string inputText = Console.ReadLine(); 
+
+string[] stringArray = inputText.Split(' '); 
+
 int[] array = new int[stringArray.Length];
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = int.Parse(stringArray[i]);    
+     array[i] = int.Parse(stringArray[i]);
 }
 
+number[i] =0;
 for (int i = 0; i < array.Length; i++)
 {
-    if (array[i] > 0)
-    Console.Write($"Введено чисел больше нуля: " + array[i] + " ");
+    if (array[i] > 0) number[i] = number[i]+1;
+    Console.Write($"Больше нуля: " + number[i] + " ");
 }
