69988a30f80d5f166b01978c2e056bc5cb5fe0d7
anatoly <a.smorodin@bk.ru>
2022-12-25 19:46:46 +0300

    Task (6)001 complete

--- Task (6)001/Program.cs
+++ Task (6)001/Program.cs
@@ -1,39 +1,24 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. 
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-// Console.WriteLine("Введите числа через пробел: ");
-// string inputText = Console.ReadLine();
-// string[] stringArray = inputText.Split(' ');
-// int[] array = new int[stringArray.Length];
+Console.WriteLine("Bведите числа через пробел: ");
+string inputText = Console.ReadLine();
+string[] stringArray = inputText.Split(' ');
+int[] Array = new int[stringArray.Length];
+int count = 0;
 
-
-// for (int i = 0; i < array.Length; i++)
-// {
-//     array[i] = int.Parse(stringArray[i]);    
-// }
-
-// for (int i = 0; i < array.Length; i++)
-// {
-//     if (array[i] > 0) 
-    
-//     Console.Write($"Введено чисел больше нуля: " + array[i] + " ");
-// }
-
-Console.WriteLine("Введите числа через пробел: ");
-string inputText = Console.ReadLine(); 
-
-string[] stringArray = inputText.Split(' '); 
-
-int[] array = new int[stringArray.Length];
-
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < stringArray.Length; i++)
 {
-     array[i] = int.Parse(stringArray[i]);
+    Array [i] = int.Parse(stringArray[i]);
 }
 
-number[i] =0;
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < Array.Length; i++)
 {
-    if (array[i] > 0) number[i] = number[i]+1;
-    Console.Write($"Больше нуля: " + number[i] + " ");
+    Console.Write(Array[i] + " ");
 }
+
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i] > 0)  count +=1;
+}
+Console.WriteLine($"Введено чисел больше нуля: " + count);
