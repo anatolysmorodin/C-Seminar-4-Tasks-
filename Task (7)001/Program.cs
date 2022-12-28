519165ac53a83ad1e432c2fc18fc0b4a8b75f8a1
anatoly <a.smorodin@bk.ru>
2022-12-28 18:03:37 +0300

    Task (7)001 complete

--- Task (7)001/Program.cs
+++ Task (7)001/Program.cs
@@ -1,48 +1,29 @@
 ﻿// Задайте двумерный массив размером m×n, 
 // заполненный случайными вещественными числами от -10,0 до 10,0.
 
-int m = new Random().Next(0, 10);
-int n = new Random().Next(0, 10);
-double[,] matrix = new double[n, m];
-
-for (int i = 0; i < matrix.GetLength(0); i++)
+void FillArrayRandomNumbers(double[,] array)
 {
-     for (int j = 0; j < matrix.GetLength(1); j++);
- }
- for (int i = 0; i < matrix.GetLength(0); i++)
- {
-     for (int j = 0; j < matrix.GetLength(1); j++)
-     {
-         matrix[i,j] = (new Random().Next(-100,101)/10.0);
-     }
-     
- }
-Console.Write(matrix);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i,j] = Convert.ToDouble(new Random().Next(-10,10)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
 
-
-// void PrintMatrix (double[,] matrix)
-// {
-// for (int i=0; i<matrix.GetLength(0); i++) 
-// {
-//     for (int j=0; j<matrix.GetLength(1); j++)
-//     {
-            
-//       if(matrix[i,j]>=0 ) Console.Write("  "+matrix[i,j]);
-//       else Console.Write(" "+matrix[i,j]);
-//     }
-//     Console.WriteLine();
-// }
-// }
-// int row = new Random().Next(3,7);
-// int column = new Random().Next(3,7);
-// double[,] Matrix =new double[row,column];
-
-// for (int i=0; i<Matrix.GetLength(0); i++) 
-// {
-//     for (int j=0; j<Matrix.GetLength(1); j++)
-//     {
-//       Matrix[i,j] = (new Random().Next(-100,101)/10.0);
-
-//     }
-// }
-// PrintMatrix(Matrix);
+double[,] numbers = new double[6,8];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+PrintArray(numbers);
